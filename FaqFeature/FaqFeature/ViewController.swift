8b71ec8051d03b89d71c04fcebe9411f696aca78
Islas <islasgiga@gmail.com>
2021-02-05 18:33:07 -0300

    create xcode project

---
+++ FaqFeature/FaqFeature/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaqFeature
+//
+//  Created by Islas Girão Garcia on 05/02/21.
+//  Copyright © 2021 Islas Girão Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
