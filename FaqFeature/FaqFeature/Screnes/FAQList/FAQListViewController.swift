205816788348d5e87f1e7a7df1425e5957856edf
Islas <islasgiga@gmail.com>
2021-02-05 18:53:26 -0300

    rename FAQListViewController, organize project folders

---
+++ FaqFeature/FaqFeature/Screnes/FAQList/FAQListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FaqListViewController.swift
+//  FaqFeature
+//
+//  Created by Islas Girão Garcia on 05/02/21.
+//  Copyright © 2021 Islas Girão Garcia. All rights reserved.
+//
+
+import UIKit
+
+class FAQListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
